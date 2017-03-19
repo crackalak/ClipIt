cb3939c19854e22c372b156a9cbf36ba0d30ba68
Dan Hughes <danhughes@rizzen.com>
2017-03-19 03:04:37 +0000

    initial commit

---
+++ ClipIt/MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ClipIt
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ctxMenuStripNotify = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ctxMenuStripNotify.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipTitle = "ClipIt";
+            this.notifyIcon.ContextMenuStrip = this.ctxMenuStripNotify;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "ClipIt";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // ctxMenuStripNotify
+            // 
+            this.ctxMenuStripNotify.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.ctxMenuStripNotify.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem,
+            this.toolStripSeparator1});
+            this.ctxMenuStripNotify.Name = "ctxMenuStripNotify";
+            this.ctxMenuStripNotify.Size = new System.Drawing.Size(191, 56);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(190, 46);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(187, 6);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(240F, 240F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(599, 354);
+            this.Name = "MainForm";
+            this.Text = "ClipIt";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ctxMenuStripNotify.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip ctxMenuStripNotify;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ ClipIt/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using EventHook;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ClipIt
+{
+    public partial class MainForm : Form
+    {
+        private ObservableCollection<KeyValuePair<Guid, string>> Clips = new ObservableCollection<KeyValuePair<Guid,string>>();
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ClipboardWatcher.OnClipboardModified += ClipboardWatcher_OnClipboardModified;
+            ClipboardWatcher.Start();
+            ctxMenuStripNotify.ItemClicked += CtxMenuStripNotify_ItemClicked;
+            Clips.CollectionChanged += Clips_CollectionChanged;
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                ShowInTaskbar = false;
+                Hide();
+            }
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ClipboardWatcher.Stop();
+        }
+
+        private void Clips_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    Invoke((Action)(() => {
+                        var item = (KeyValuePair<Guid, string>)e.NewItems[0];
+
+                        // limit the size of the menu item
+                        string formattedText = item.Value.Replace(Environment.NewLine, " ");
+                        if (formattedText.Length > 20)
+                            formattedText = $"{formattedText.Substring(0, 20)}...";
+                        
+                        ctxMenuStripNotify.Items.Add(new ToolStripMenuItem(formattedText) { Tag = item.Key });
+                    }));
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    Invoke((Action)(() => {
+                        var item = (KeyValuePair<Guid, string>)e.OldItems[0];
+                        
+                        // remove the menu item by guid
+                        var menu = ctxMenuStripNotify.Items.OfType<ToolStripMenuItem>()
+                                    .Where(i => i.Tag != null && (Guid)i.Tag == item.Key)
+                                    .FirstOrDefault();
+
+                        ctxMenuStripNotify.Items.Remove(menu); 
+                    }));
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void CtxMenuStripNotify_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem != quitToolStripMenuItem)
+            {
+                // get the full item
+                var item = Clips.Where(c => c.Key == (Guid)e.ClickedItem.Tag).FirstOrDefault();
+
+                ClipboardWatcher.Stop();
+                Clipboard.SetText(item.Value);
+                ClipboardWatcher.Start();
+                
+                // remove it from the current position and add to the end (latest)
+                Clips.Remove(item);
+                Clips.Add(item);
+            }
+        }
+
+        private void ClipboardWatcher_OnClipboardModified(object sender, ClipboardEventArgs e)
+        {
+            Clips.Add(new KeyValuePair<Guid, string>(Guid.NewGuid(), (string)e.Data));
+        }
+        
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            Show();
+            ShowInTaskbar = true;
+        }
+    }
+}
